d5695317692e1541d98c1bfd8bb655378167d68a
Reva Mahto <reva.mahto@gmail.com>
2024-05-06 19:09:51 -0700

    Added routing to pages in navbar. Created new files for other components.

--- src/App.js
+++ src/App.js
@@ -3,7 +3,10 @@ import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Welcome from './components/Welcome';
+import About from './components/About';
+import Contact from './components/Contact';
 import UpcomingEvents from './components/UpcomingEvents';
+import Resources from './components/Resources';
 
 
 const App = () => {
@@ -11,8 +14,11 @@ const App = () => {
     <Router>
       <Navbar />
       <Routes>
-        <Route path="/" element={<Welcome />} />
+        <Route path="/" element={<Welcome />} />  {/* landing page */}
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
         <Route path="/events" element={<UpcomingEvents />} />
+        <Route path="/resources" element={<Resources />} />
       </Routes>
     </Router>
   );
