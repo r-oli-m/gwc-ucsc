572cb5e4e73eeab5c0c4075a4e8790d0d0a2942f
Reva Mahto <reva.mahto@gmail.com>
2024-05-06 19:09:51 -0700

    Added routing to pages in navbar. Created new files for other components.

---
+++ src/components/Contact.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Contact = () => {
+  return (
+    <div>
+      <h1>Contact Component</h1>
+      {/* Add your contact form or other content here */}
+    </div>
+  );
+};
+
+export default Contact;
