37ab6cab9a7e17ada6b1f12942c3f3bf3df7b214
Reva Mahto <reva.mahto@gmail.com>
2024-05-07 18:50:40 -0700

    updated bg color

--- src/components/Resources.jsx
+++ src/components/Resources.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
-import InstagramEmbed from './InstagramProfile';
 import '../styles/Resources.css';
 
 const Resources = () => {
   return (
     <div className='resources'>
       <h1>Resources</h1>
-      <InstagramEmbed url="https://www.instagram.com/p/C6FRqBCPv2J/" /> {/* pass the insta post url u wanna see*/}
+      
     </div>
   );
 };
