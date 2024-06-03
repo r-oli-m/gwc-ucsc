b3c0b9d33096e42b65de601d19bffdf727ed07a7
meghagl17 <megha.agl17@gmail.com>
2024-06-02 17:47:16 -0700

    contant page almost done

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,12 +1,43 @@
 import React from 'react';
 import '../styles/Contact.css';
 import InstagramEmbed from './InstagramProfile';
+import { FaLinkedin } from "react-icons/fa";
+
+import Members from './Members'
+
+const partialList = (array, size) => {
+  const oneRow = [];
+  for (let i = 0; i < array.length; i += size) {
+    oneRow.push(array.slice(i, i + size));
+  }
+  return oneRow;
+};
 
 const Contact = () => {
+  const membersList = partialList(Members, 4);
+
   return (
     <div className='contact'>
-      <h1>Contact Component</h1>
-      <InstagramEmbed url="https://www.instagram.com/p/C6FRqBCPv2J/" /> {/* pass the insta post url u wanna see*/}
+      <h1 className="heading" >Our Team</h1>
+      {/* <InstagramEmbed url="https://www.instagram.com/p/C6FRqBCPv2J/" /> */}
+
+      <div>
+        {membersList.map((row, rowIndex) => (
+          <div className="row" key={rowIndex}>
+            {row.map((Member, itemIndex) => (
+              <div className="item" key={itemIndex}>
+                <div className = "memberPicture"><img src={Member.picture} /></div>
+                <div className = "memberLinkedin"><a href={Member.linkedin} target="_blank" >
+                    <FaLinkedin size={30} />
+                </a></div>
+                <div className = "memberName">{Member.name}</div>
+                <div className = "memberPosition">{Member.position}</div>
+              </div>
+            ))}
+          </div>
+        ))}
+      </div>
+
     </div>
   );
 };
---
+++ src/components/Members.js
@@ -0,0 +1,33 @@
+const Members = [
+    {'picture': 'https://via.placeholder.com/150', 
+    'linkedin': 'https://www.linkedin.com/in/megha-agarwal-4b14b3268/',
+    'name': 'Megha Agarwal',
+    'position': 'Website Developer'
+    }, 
+    {'picture': 'https://via.placeholder.com/150', 
+    'linkedin': 'url',
+    'name': 'name',
+    'position': 'Developer'
+    },
+    {'picture': 'https://via.placeholder.com/150', 
+    'linkedin': 'url',
+    'name': 'name',
+    'position': 'Developer'
+    }, 
+    {'picture': 'https://via.placeholder.com/150', 
+    'linkedin': 'url',
+    'name': 'name',
+    'position': 'Developer'
+    }, 
+    {'picture': 'https://via.placeholder.com/150', 
+    'linkedin': 'url',
+    'name': 'name',
+    'position': 'Developer'
+    }, 
+    {'picture': 'https://via.placeholder.com/150', 
+    'linkedin': 'url',
+    'name': 'name',
+    'position': 'Developer'
+    }];
+
+export default Members;
